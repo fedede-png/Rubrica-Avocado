baa647185e53887e7982034ad3dbd0d48e9a136a
ernec <ernec@LAPTOP-NNBU419O.station>
2021-03-31 13:33:44 +0200

    Initial commit

--- src/it/edu/gastaldiabba/rubrica/controller/FXMLDocumentController.java
+++ src/it/edu/gastaldiabba/rubrica/controller/FXMLDocumentController.java
@@ -2,17 +2,22 @@
 package it.edu.gastaldiabba.rubrica.controller;
 
 import it.edu.gastaldiabba.rubrica.model.Cliente;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 /**
  *
@@ -72,5 +77,19 @@ public class FXMLDocumentController implements Initializable {
         table.setItems(clientiOss);
         
     }    
-    
+    @FXML
+        private void aggMet(ActionEvent event) throws IOException
+    {
+         try {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/it.edu.gastaldiabba.rubrica.view.FXMLAggiungi.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));  
+        stage.show();
+    } catch(Exception e) {
+        e.printStackTrace();
+    }
+        System.out.println("lollolol");
+    }
+
 }
